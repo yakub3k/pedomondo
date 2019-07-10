e7584c58db414d7de3bb30572a6cb36d9f968281
yakub3k <yakub3k+git@gmail.com>
2019-07-11 00:05:58 +0200

    fix mail addresses

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO users VALUES (1, 'yakub', 'pass','yakub3k@gmail.com');
-INSERT INTO users VALUES (2, 'alexander','pass', 'alex@gmail.com');
-INSERT INTO users VALUES (3, 'jolo','pass', 'jolo@gmail.com');
+INSERT INTO users VALUES (1, 'yakub', 'pass','yakub@yakub.com');
+INSERT INTO users VALUES (2, 'alexander','pass', 'alex@yakub.com');
+INSERT INTO users VALUES (3, 'jolo','pass', 'jolo@yakub.com');
 
 -- INSERT INTO books VALUES (1, 'Book title ','Author');
